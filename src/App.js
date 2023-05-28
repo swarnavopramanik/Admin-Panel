7ceba42dbee3eb3810d866587fa8f141f512e4d7
akhilesh <akhileshkumar@gofynd.com>
2023-05-28 23:40:53 +0530

    added feedback section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import Feedback from './components/Feedback';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
@@ -61,6 +62,10 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
+            <div className="feedback-section">
+              <Feedback/>
+            </div>
+
             <div>
               {themeSettings && (<ThemeSettings />)}
 
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import emailjs from 'emailjs-com';
+
+
+const Feedback = () => {
+      const [feedback,setFeedBack] = useState('');
+      const [submitted,setSubmitted] = useState(false);
+      const[email,setEmail] = useState('');
+      emailjs.init('1AkXF-PC-6FG2Ym15')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const templateParams ={
+            feedback:feedback,
+            from_email:email,
+            to_email:'swarnavo.pramanik8697@gmail.com',
+        };
+        emailjs
+        .send('service_c8wox7q', 'template_47lh3vm', templateParams,'1AkXF-PC-6FG2Ym15')
+    .then((response) => {
+      console.log('Feedback sent!', response.status, response.text);
+      setSubmitted(true);
+      setFeedBack('');
+      setEmail('')
+    })
+    .catch((error) => {
+      console.error('Error sending feedback:', error);
+    });
+
+
+    };
+  return (
+
+    <div>
+        <form onSubmit={handleSubmit} >
+            <textarea placeholder="enter your feedback"
+            style = {{border: '2px solid #ccc',boxSizing:'border-box',borderRadius:'4px',resize:'none',width:'50%',height:'100px'}}
+            value = {feedback}
+            onChange = {(e)=>setFeedBack(e.target.value)}>
+
+            </textarea>
+            <input
+            name = "email"
+            style ={{display:'flex',alignItems:'right',border: '2px solid #ccc',width:'50%',height:'50px',borderRadius:'4px' }}
+            placeholder='enter your email'
+            value = {email}
+            onChange = {(e)=>setEmail(e.target.value)}
+            />
+
+            <button type = "submit" onClick={handleSubmit} style={{borderRadius:'4px',border:'1px solid #ccc',height:'40px',width:'100px',marginTop:'30px'}}>Submit</button>
+            </form>
+            {submitted &&(
+                <div>
+                     <p>Sent!</p>
+                </div>
+            )}
+    </div>
+  )
+};
+export default Feedback;
